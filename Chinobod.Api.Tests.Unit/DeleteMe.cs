b209970ed4db0427e9133e2d357f679313d67c15
Golibjon <113083394+GolibjonTuxtamatov@users.noreply.github.com>
2024-07-05 16:58:06 +0500

    INFRA: Initialize xUnit Project

---
+++ Chinobod.Api.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace Chinobod.Api.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() =>
+            Assert.True(true);
+            
+    }
+}
